f61887629f8154977323a36a2b0227f9c2921b5e
Erich <EricHamelin89@users.noreply.github.com>
2022-09-01 11:27:46 -0400

    Pushing to Main / fixed checkout
    2nd submission

---
+++ SushiEShop.Web/TagHelpers/PriceTagHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Globalization;
+
+namespace SushiEShop.Web.TagHelpers
+{
+    public class PriceTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.AppendHtml(@NumberFormatInfo.CurrentInfo.CurrencySymbol);
+        }
+    }
+}
